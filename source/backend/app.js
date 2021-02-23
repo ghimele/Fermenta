0f14f15ff268760e9ad237d3cc81b6af66b338c8
Michele Vomera <michele.vomera@outlook.com>
2021-02-23 16:33:11 +0100

    start backend server

---
+++ source/backend/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.use('/api*', indexRouter);
+
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+module.exports = app;
